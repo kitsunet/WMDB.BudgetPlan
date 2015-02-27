a61ae8e5accc273953914c4cdc241c4b5778b3a2
Mathias Schreiber <mathias.schreiber@wmdb.de>
2015-02-27 15:12:19 +0100

    [BUGFIX] Fixed the Page Markup

---
+++ Resources/Private/Javascripts/WMDB.BudgetPlan.js
@@ -0,0 +1,8 @@
+//@prepros-prepend ../Libraries/bower_components/jquery/dist/jquery.js
+//@prepros-prepend ../Libraries/bower_components/foundation/js/foundation.js
+//@prepros-prepend ../Libraries/bower_components/foundation/js/foundation/foundation.abide.js
+//@prepros-prepend ../Libraries/bower_components/foundation/js/foundation/foundation.reveal.js
+//@prepros-prepend ../Libraries/bower_components/foundation/js/foundation/foundation.topbar.js
+//@prepros-prepend ../Libraries/bower_components/foundation/js/foundation/foundation.alert.js
+
+$(document).foundation();
